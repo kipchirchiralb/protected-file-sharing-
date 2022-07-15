8dc1440ce7c68691c7d79e4ccfc8d04c53320a91
Kipchirchir Albert <albertkipchirchir02@gmail.com>
2022-07-15 18:18:04 +0300

    object array object array

--- server.js
+++ server.js
@@ -7,23 +7,35 @@ const bcrypt = require('bcrypt');
 const File = require('./models/File')
 
 
-const upload = multer({dest: "uploads/"})
+const upload = multer({dest: "public/uploads/"})
 const PORT  = process.env.PORT || 3000
 mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 
 app.get("/",async (req,res)=>{
-    const files = await File.find()
-    // add req.headers.origin to all file paths
-    // console.log(files)
-    files.forEach(file=>{
-        // file.fileLink= `${req.headers.origin}`
-        // console.log(typeof file.path)
-        // replace \ for each path with / 
+    let files = await File.find()
+    let newFiles =  Object.entries(files)
+    // files.forEach(file=>{
+    //     filePaths.push(file.path.slice(6))
+    //     console.log(file.path)
+    //     console.log(file.originalName)
+    //     // file.path= `/${file.path.replace('\\', '/')}`
+    // })
+    let filePaths = []
+    let fileNames = []
+    newFiles.forEach(newFile=>{
+        filePaths.push(newFile[1].path.slice(15))
+        fileNames.push(newFile[1].originalName)
     })
-    res.render("index", {files: files})
+    filePaths = filePaths.toString()
+    fileNames = fileNames.toString()
+    // filePaths = filePaths.split(',')[0]
+    // fileNames = fileNames.split(',')[0]
+    // console.log(filePaths, fileNames)
+    res.render("index", {fileNames: fileNames, filePaths: filePaths})
 })
 app.post("/upload", upload.single("file"), async (req,res)=>{
     const fileData = {
@@ -35,10 +47,18 @@ app.post("/upload", upload.single("file"), async (req,res)=>{
     }
     const file = await File.create(fileData)
     // console.log(file)
+    // render an empty file when one is needed to deletemall afa
     res.render("index", {fileLink: `${req.headers.origin}/uploads/${file.id}`})
 })
 // app.get("/uploads/:id", handleDownload)
 // app.post("/uploads/:id", handleDownload)
+
+app.post("/clear", async (req,res)=>{
+    await File.deleteMany({})
+    const files = await File.find()
+    console.log(files)
+    res.redirect('/')
+})
 app.route("/uploads/:id").get(handleDownload).post(handleDownload)
 
 async function handleDownload(req, res) {
